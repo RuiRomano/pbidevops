b4b44188f6c776d4c5fd2e39d5d959e1af44a8c8
mike carlo <mike@powerbi.tips>
2021-09-29 12:21:45 -0500

    add(docs): Add notes and documentation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # pbidevops
+
+
+config.json
+deployOptions
+- permissions removal
+
+shareddatasets.json
+
+Publish reports commands
+filter command in PS 
+```powershell
+# Deploy Reports
+
+Publish-PBIReports -configPath $configPath -path "$projectPath\Reports" -filter @("Customer.pbix","Purchases.pbix")
+```
--- deploy.ps1
+++ deploy.ps1
@@ -15,17 +15,20 @@ $configPath = "$currentPath\config.json"
 Connect-PowerBIServiceAccount
 
 # Deploy Workspaces
-
 Publish-PBIWorkspaces -configPath $configPath
 
 # Deploy Datasets
-
 Publish-PBIDataSets -configPath $configPath -path "$projectPath\DataSets"
+
+# Rebind Reports locally (Un-Supported)
+# Set-PBIReportConnections
 
 # Deploy Reports
 # README - The live connected PBIX reports need to be binded to an existent Dataset on powerbi.com - Run tool.FixReportConnections.ps1 to fix the pbix connections
-Publish-PBIReports -configPath $configPath -path "$projectPath\Reports"
+# Optional use the parameter '-filter @("Customer.pbix","Purchases.pbix")' to selectively deploy reports. 
+# -- without the '-filter' option the Publish-PBIReports will publish all reports into the service from the Reports folder
+# -- Note: the @("","") syntax is how you pass in an array in PowerShell
+Publish-PBIReports -configPath $configPath -path "$projectPath\Reports" # -filter @("Customer.pbix")
 
 # Deploy PaginatedReports
-
 # Publish-PBIReports -configPath $configPath -path "$projectPath\PaginatedReports"
